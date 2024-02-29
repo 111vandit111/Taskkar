06c01455f4103959c6ef56d4e4d3f57a951bf0a4
111vandit111 <vanditkala@gmail.com>
2024-02-29 21:06:41 +0530

    chores: changed sidebar a little

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,9 @@
 const Sidebar = () => {
-    return (
-        <div>
-            Sidebar
-        </div>
-    )
-}
+  return (
+    <aside className="fixed z-[1] left-0 bg-cyan-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-[#efefef]">
+      Side!
+    </aside>
+  );
+};
 
 export default Sidebar;
